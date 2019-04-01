483dd03fafca3fbbe1a0471e2d31bcc901b2d018
HSNBDES33\eocariz <eocariz@hasar.com>
2019-04-01 09:53:02 -0300

    creación de app

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// Función Flecha o Arrow Function
+const App = () => {
+    // Lo que ejecuta la función
+    return <h1> Hola Mundo (app) </h1>
+}
+
+export default App
+
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+// importar el componente
 
+// utilizar el componente
 ReactDOM.render(<h1> Hola Mundo </h1>,
     document.getElementById('root'))
