65d6ea897c43ef1dda3074ada814a6f0ffb7af17
HSNBDES33\eocariz <eocariz@hasar.com>
2019-04-02 11:09:53 -0300

    Antipatron SOLID

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,35 @@ const App = () => {
     console.log("Renderización de App")
     return (
     <main className='react-calculator'>
-        
+        <div className="result">
+        </div>
+        <div className="numbers">
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>0</button>
+        </div>
+        <div className="functions">
+            <button>
+                clear
+            </button>
+            <button>
+                r
+            </button>
+        </div>
+        <div className="math-operations">
+            <button>+</button>
+            <button>-</button>
+            <button>*</button>
+            <button>/</button>
+            <button>=</button>
+        </div>
     </main>)
 }
 
