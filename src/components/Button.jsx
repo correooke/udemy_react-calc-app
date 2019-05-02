7112ccb3b120d57e450f2254a0c6bdce2999f550
HSNBDES33\eocariz <eocariz@hasar.com>
2019-05-01 21:48:05 -0300

    Simplificación de sintaxis

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,18 @@
 // importar React
 import React from 'react'
+import PropTypes from 'prop-types'
 
 // Componente Funcional
-const Button = ({type, text, clickHandler}) => {
-    return (
-        <button className={type} onClick={() => {
-            console.log("onClick en el Button")
-            clickHandler(text)
-        }}>
-            <span>{text}</span>
-        </button>
-    )
-}
+const Button = ({type, text, clickHandler}) => (
+    <button className={type} onClick={() => clickHandler(text)}>
+        <span>{text}</span>
+    </button>
+)
+
+Button.propTypes = {
+    type: PropTypes.string,
+    text: PropTypes.string.isRequired,
+    clickHandler: PropTypes.func.isRequired
+} 
 
 export default Button
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Result = ({ value }) => {
-    console.log("Renderización de Result", value)
-    return (
-        <div className="result">
-            {value}
-        </div>
-    )
-}
+const Result = ({ value }) => (
+    <div className="result">
+        {value}
+    </div>
+)
 
 Result.propTypes = {
     value: PropTypes.string.isRequired
