0ca996d53f2e8d4728b66f2f2c19faf928b560ba
HSNBDES33\eocariz <eocariz@hasar.com>
2019-05-04 21:28:13 -0300

    Utilizando la función map en renderButtons

--- src/components/Numbers.jsx
+++ src/components/Numbers.jsx
@@ -3,11 +3,17 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 
 // DRY: Don't repeat yourself
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 const renderButtons = onClickNumber => {
     // var number = 0
     // iterar desde el botón 1 al 0 (while, for, foreach)
     // var ArrayComponentes += 
     //<Button text={number.toString()} clickHandler={onClickNumber} />
+    const renderButton = number => (
+        <Button text={number.toString()} clickHandler={onClickNumber} />
+    )
+    return numbers.map(renderButton)
 }
 
 const Numbers = ({ onClickNumber }) => (
