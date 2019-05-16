3149387158cdba439b8a409c2e0b9d62e9a89ee4
HSNBDES33\eocariz <eocariz@hasar.com>
2019-05-16 14:17:52 -0300

    Propiedad Key Practica

--- src/components/Numbers.jsx
+++ src/components/Numbers.jsx
@@ -11,7 +11,11 @@ const renderButtons = onClickNumber => {
     // var ArrayComponentes += 
     //<Button text={number.toString()} clickHandler={onClickNumber} />
     const renderButton = number => (
-        <Button text={number.toString()} clickHandler={onClickNumber} />
+        <Button 
+            key={number} 
+            text={number.toString()} 
+            clickHandler={onClickNumber} 
+        />
     )
     return numbers.map(renderButton)
 }
